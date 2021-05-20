6c80f1c9c044343b80474a5cd4641df101926255
ankitwest <anshkrrr@gmail.com>
2021-05-20 09:34:41 +0530

    AB Tube Toutube clone app

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import HomeScreen from "./src/HomeScreen";
+import LibraryScreen from "./src/LibraryScreen";
+import SubscriptionScreen from "./src/SubscriptionScreen";
+import TrendingScreen from "./src/TrendingScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: HomeScreen,
+    Trending: TrendingScreen,
+    Subscription: SubscriptionScreen,
+    Library: LibraryScreen
   },
-});
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName = "home";
+        if (routeName === "Library") {
+          iconName = "folder";
+        }
+        switch (routeName) {
+          case "Home":
+            iconName = "home";
+            break;
+          case "Library":
+            iconName = "folder";
+            break;
+          case "Subscription":
+            iconName = "message-video";
+            break;
+          case "Trending":
+            iconName = "fire";
+            break;
+        }
+
+        return <Icon name={iconName} size={25} color={tintColor} />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: "tomato",
+      inactiveTintColor: "gray"
+    }
+  }
+);
+
+export default createAppContainer(TabNavigator);
---
+++ src/TrendingScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+function TrendingScreen() {
+    return <View style="styles.container"></View>;
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'green',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+  });
+  
+  export default TrendingScreen;
